33def3a4865ddd1f6301b63cd203b6cb59d56179
muriz-dev <mhmdrizki.dev@gmail.com>
2023-10-27 10:41:21 +0700

    Tampilan tambah kategori

---
+++ pages/kategori/tambah_kategori.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Kategori</title>
+</head>
+<body>
+    <form action="../../backend/kategori/act_create.php" method="get">
+        <label>Nama Kategori: </label><input type="text" name="nama_kategori">
+        <br><br>
+        <button type="submit">Simpan</button>
+    </form>
+</body>
+</html>
